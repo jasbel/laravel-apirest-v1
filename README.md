702c7e01b0a00a2973326fa4346b2a2176e0af08
jasbel <asbel.elmec@gmail.com>
2021-04-02 11:47:47 -0400

    Final Version 2 apirest, ADD authentication and more

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 php artisan make:controller Api/V1/PostController --api --model=Post
 
 php artisan make:resource V1/PostResource
+
+
+version 2
+================
+
+php artisan make:controller Api/V2/PostController --api --model=Post
+php artisan make:resource V2/PostResource
+php artisan make:resource V2/PostCollection
+
+<!-- Instalando nueva herramienta -->
+composer require laravel/sanctum
+php artisan migrate
+
+<!-- en postman -->
+headers
+accept      application/json
+
+php artisan make:controller Api/LoginController
--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 
-Route::apiResource('v1/posts',App\Http\Controllers\Api\V1\PostController::class)
+// Apirest V1
+Route::apiResource('v1/posts',PostV1::class)
     ->only(['index','show', 'destroy']);
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+// Apirest V2
+Route::apiResource('v2/posts',PostV2::class)
+    ->only(['index','show'])
+    ->middleware('auth:sanctum');
+
+Route::post('login', [
+    App\Http\Controllers\Api\LoginController::class,
+    'login',
+]);
